16c4a6f97a9f463c6a09c48796196990c7fd1837
Sacha <48031121+SachaBog@users.noreply.github.com>
2020-05-30 13:50:33 +0200

    updating walking, adding neediness prop

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -3,7 +3,11 @@
 		<Tamagotchi class ="tamagotchi"
 			v-for="cow in activeCows"
 			:key="cow.name"
-			:name="cow.name">
+			:name="cow.name"
+			:sleepiness="cow.sleepiness"
+			:speed="cow.speed"
+			:neediness="cow.neediness"
+			:command="command">
 		</Tamagotchi>
   </div>
 </template>
@@ -19,13 +23,14 @@ export default {
 	data () {
 		return {
 			cows: [
-				{name: 'Billy', active: true},
-				{name: 'Madeliefje', active: true},
-				{name: 'Ellie', active: true},
-				{name: 'Tommie', active: true},
-				{name: 'Pieter', active: true},
-				{name: 'Anna', active: true}
-			]
+				{name: 'Billy', active: true, sleepiness: 5, speed: 7, size: 1, neediness: 2},
+				{name: 'Madeliefje', active: true, sleepiness:0, speed: 10, size: 1, neediness: 8},
+				{name: 'Ellie', active: true, sleepiness: 6, speed: 5, size: 1, neediness: 10},
+				{name: 'Tommie', active: true, sleepiness: 2, speed: 8, size: 1, neediness: 10},
+				{name: 'Pieter', active: true, sleepiness: 0.5, speed: 4, size: 1, neediness: 4},
+				{name: 'Anna', active: true, sleepiness: 10, speed: 3, size: 2, neediness: 1}
+			],
+			command: ''
 		}
 	},
 	computed: {
--- src/components/Tamagotchi.vue
+++ src/components/Tamagotchi.vue
@@ -1,9 +1,19 @@
 <template>
-	<div @click="petCow" class="tamagotchi" ref="tamagotchi" v-bind:style="styleObject">
-		<img :src="image_url"/>
+	<div @click="petCow"  
+		class="tamagotchi" 
+		ref="tamagotchi" 
+		:style="{left: this.x + '%', top: this.y + '%'}">
+		<div
+			class="tamagotchi__display"
+			:class="{'tamagotchi__display--flipped': direction=='right'}">
+			<img 
+				class="tamagotchi__img" 
+				:class="{'tamagotchi__img--walking': walking}"
+				:src="image"/>
+		</div>
 		<p>{{name}}</p>
 		<p>Happiness: {{happiness}}</p>
-		<p>Mood: {{mood}}</p>
+		<p>Status: {{status}}</p>
 	</div>
 </template>
 
@@ -12,62 +22,93 @@ import { gsap } from 'gsap'
 
 export default {
 	props: {
-		name: String
+		name: String,
+		sleepiness: Number,
+		speed: Number,
+		neediness: Number
 	},
 	data () {
 		return {
-			happiness: 10,
-			mood: 'happy',
-			speed: 8,
-			styleObject: {
-				left: '50%',
-				top: '50%'
+			happiness: 100,
+			status: 'happy',
+			// left: 50,
+			// top: 50,
+			x: 80*Math.random(),
+			y: 80*Math.random(),
+			direction: 'left',
+			walking: false
+		}
+	},
+	watch: {
+		status: function(newStatus) {
+			if (newStatus == 'dead') {
+				this.walking = false
 			}
 		}
 	},
 	methods: {
 		petCow() {
 			this.happiness = this.happiness + 5
-			this.updateMood()
+			this.updateStatus()
 		},
-		updateStats () {
-			this.happiness = Math.max(0, this.happiness - 1)
-			this.updateMood()
-			setTimeout(() => { this.updateStats() }, 1500)
-		},
-		updateMood () {
+		// updateStats () {
+		// 	this.happiness = Math.max(0, this.happiness - 1)
+
+		// 	setTimeout(() => { this.updateStats() }, 1500)
+		// },
+		updateStatus () {
 			if (this.happiness > 15) {
-				this.mood = 'superHappy'
+				this.status = 'superHappy'
 			} else if (this.happiness > 7) {
-				this.mood = 'happy'
+				this.status = 'happy'
 			} else if (this.happiness > 4) {
-				this.mood = 'neutral'
+				this.status = 'neutral'
 			} else if (this.happiness > 0) {
-				this.mood = 'sad'
+				this.status = 'sad'
 			} else {
-				this.mood = 'dead'
+				this.status = 'dead'
 			}
 		},
 		walk () {
+			if (this.status != 'dead') {
+				let x = this.x + (20*Math.random()- 10)
+				x = Math.max(0, x)
+				x = Math.min(80, x)
+				let y = this.y + (20*Math.random()- 10)
+				y = Math.max(0, y)
+				y = Math.min(80, y)
+				if (x > this.x) {
+					this.direction = "right"
+				} else {
+					this.direction = "left"
+				}
+				// const walkDistance = Math.sqrt((this.x - x)^2 + (this.y - y)^2)
+				this.walking = true
+				this.x = x
+				this.y = y
+				setTimeout(() => this.walking = false, 2000)
+			}
+			setTimeout(this.walk, 5000*Math.random() + 2000)
+		},
+		walkGsap () {
 		const { tamagotchi } = this.$refs
-		const tl = gsap.timeline({
-		})
-		console.log(this.name)
-		tl.to(tamagotchi, 3, {left: "random(0, 100)%", top: 'random(0, 100)%'})
-			.to(tamagotchi, 3, {left: "random(0, 100)%", top: "random(0, 100)%"}, "+=5")
-			.to(tamagotchi, 3, {left: "random(0, 100)%", top: "random(0, 100)%"})
+		gsap.to(tamagotchi, {repeat: -1, delay: this.sleepiness, duration: 11 - this.speed, left: "+=random(-20, 20)%" , top: '+=random(-20, 20)%', repeatRefresh: true})
 		}
 	},
 	computed: {
-		image_url () {
-			let image_name = this.mood + 'Cow'
-			var image = require('../assets/' + image_name + '.png')
+		image () {
+			let image_name = this.status + 'Cow'
+			let image = require('../assets/' + image_name + '.png')
 			return image
 		},
 	},
 	mounted () {
-		this.updateStats()
-		this.walk()
+		setInterval(() => {
+			this.happiness = Math.max(0, this.happiness - 1)
+			this.updateStatus()
+		}, 1000 + (11 - this.neediness) * 300)
+		// this.updateStats()
+		setTimeout(this.walk, Math.random()*3000 + 2000)
 	}
 }
 </script>
@@ -75,7 +116,25 @@ export default {
 <style scoped>
 .tamagotchi {
 	position: absolute;
+	transition: left 2s ease-in-out, top 2s ease-in-out;
 }
+	.tamagotchi__display--flipped {
+		transform: scaleX(-1);
+	}
+	.tamagotchi__img {
+		animation: bounce 0.5s infinite alternate;
+	}
+		.tamagotchi__img--walking {
+			animation: bounce 0.1s infinite alternate;
+		}
 
+@keyframes bounce {
+	from {
+		transform: translateY(0px);
+	}
+	to {
+		transform: translateY(-3px);
+	}
+}
 
 </style>
